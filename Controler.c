a884a7ae18d550dd967e60b1be38fb4d23082103
guicouto13 <50532744+guicouto13@users.noreply.github.com>
2021-05-30 18:20:43 -0300

    Update Controler.c

--- Controler.c
+++ Controler.c
@@ -1,24 +1,3 @@
-/*
- * Copyright 1996-2020 Cyberbotics Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/*
- * Description:  Default controller of the e-puck robot
- */
-
-/* include headers */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,7 +9,7 @@
 #include <webots/nodes.h>
 #include <webots/robot.h>
 
-/* Device stuff */
+
 #define DISTANCE_SENSORS_NUMBER 8
 static WbDeviceTag distance_sensors[DISTANCE_SENSORS_NUMBER];
 static double distance_sensors_values[DISTANCE_SENSORS_NUMBER];
@@ -55,10 +34,9 @@ static WbDeviceTag left_motor, right_motor;
 static double speeds[2];
 
 int main(int argc, char **argv) {
-  // initialize the Webots API
+
   wb_robot_init();
 
-  // internal variables
   int i;
   WbDeviceTag ps[8];
   char ps_names[8][4] = {
@@ -66,7 +44,6 @@ int main(int argc, char **argv) {
     "ps4", "ps5", "ps6", "ps7"
   };
 
-  // initialize devices
   for (i = 0; i < 8 ; i++) {
     ps[i] = wb_robot_get_device(ps_names[i]);
     wb_distance_sensor_enable(ps[i], TIME_STEP);
@@ -79,14 +56,11 @@ int main(int argc, char **argv) {
   wb_motor_set_velocity(left_motor, 0.0);
   wb_motor_set_velocity(right_motor, 0.0);
 
-  // feedback loop: step simulation until an exit event is received
   while (wb_robot_step(TIME_STEP) != -1) {
-    // read sensors outputs
     double ps_values[8];
     for (i = 0; i < 8 ; i++)
       ps_values[i] = wb_distance_sensor_get_value(ps[i]);
 
-    // detect obstacles
     bool right_obstacle =
       ps_values[0] > 100.0 ||
       ps_values[1] > 100.0 ||
@@ -96,11 +70,9 @@ int main(int argc, char **argv) {
       ps_values[6] > 100.0 ||
       ps_values[7] > 100.0;
 
-    // initialize motor speeds at 50% of MAX_SPEED.
     double left_speed  = 0.5 * MAX_SPEED;
     double right_speed = 0.5 * MAX_SPEED;
 
-    // modify speeds according to obstacles
     if (left_obstacle) {
       // turn right
       left_speed  = 0.5 * MAX_SPEED;
@@ -112,12 +84,10 @@ int main(int argc, char **argv) {
       right_speed = 0.5 * MAX_SPEED;
     }
 
-    // write actuators inputs
     wb_motor_set_velocity(left_motor, left_speed);
     wb_motor_set_velocity(right_motor, right_speed);
   }
 
-  // cleanup the Webots API
   wb_robot_cleanup();
-  return 0; //EXIT_SUCCESS
+  return 0;
 }
